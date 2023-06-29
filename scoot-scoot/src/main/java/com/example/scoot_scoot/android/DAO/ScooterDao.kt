d2f23f1045356afdaad2147add53393799db5462
Lukas Pillibeit <lukas.pillibeit@innogames.com>
2023-06-29 13:20:37 +0200

    adds more querys

--- scoot-scoot/src/main/java/com/example/scoot_scoot/android/DAO/ScooterDao.kt
+++ scoot-scoot/src/main/java/com/example/scoot_scoot/android/DAO/ScooterDao.kt
@@ -2,6 +2,7 @@ package com.example.scoot_scoot.android.DAO
 
 import androidx.room.Dao
 import androidx.room.Delete
+import androidx.room.Query
 import androidx.room.Upsert
 import com.example.scoot_scoot.android.Entity.Scooter
 
@@ -13,4 +14,17 @@ interface ScooterDao {
 
     @Delete
     fun deleteScooter(scooter: Scooter )
+
+    @Query("SELECT * FROM Scooter WHERE name LIKE :name")
+    fun findByName(name: String): Scooter
+
+    @Query("SELECT id FROM Scooter WHERE x LIKE :x AND y LIKE :y")
+    fun findByPosition(x: Int, y: Int): Scooter
+    @Query("SELECT * FROM Scooter")
+    fun getAll(): List<Scooter>
+
+    @Query("SELECT priceMin FROM Scooter WHERE id LIKE :id")
+    fun getPriceByMin(id:Int): Int
+    @Query("SELECT priceKm FROM Scooter WHERE id LIKE :id")
+    fun getPriceByKM(id: Int): Int
 }
