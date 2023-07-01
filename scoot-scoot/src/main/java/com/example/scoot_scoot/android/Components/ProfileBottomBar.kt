ff743fd21b1337791b821dc97adcfcbaa3e924d5
Anna Zitzer <an.zitzer@gmail.com>
2023-07-01 10:22:17 +0200

    add profile screen bottom navigation bar

---
+++ scoot-scoot/src/main/java/com/example/scoot_scoot/android/Components/ProfileBottomBar.kt
@@ -0,0 +1,66 @@
+package com.example.scoot_scoot.android.Components
+
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.scoot_scoot.android.R
+
+object ProfileBottomBar {
+    @Composable
+    fun ProfileBottomBar(navController: NavController) {
+        BottomNavigation(
+            modifier = Modifier.fillMaxHeight(),
+            backgroundColor = MaterialTheme.colors.primary,
+            contentColor = MaterialTheme.colors.onSecondary
+        ) {
+            BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.money),
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
+            },
+                label = { Text(text = "Balance") }
+            )
+            BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.history),
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
+            },
+                label = { Text(text = "Trips") }
+            )
+            BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
+            },
+                label = { Text(text = "Account") }
+            )
+            BottomNavigationItem(selected = false, onClick = { /*TODO*/ }, icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.help),
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
+            },
+                label = { Text(text = "Help") }
+            )
+        }
+
+    }
+}
--- scoot-scoot/src/main/java/com/example/scoot_scoot/android/Screens/ProfileScreen.kt
+++ scoot-scoot/src/main/java/com/example/scoot_scoot/android/Screens/ProfileScreen.kt
@@ -14,39 +14,51 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.scoot_scoot.android.Components.ProfileBottomBar.ProfileBottomBar
+import com.example.scoot_scoot.android.R
 
 object ProfileScreen {
     @Composable
     fun ProfileScreen(navController: NavController) {
         //TODO: Add clickability and option to edit and move to own component
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top,
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
+        Scaffold(
+            bottomBar = {
+                BottomAppBar(modifier = Modifier.height(70.dp)){ ProfileBottomBar(navController)}
+            }
+        ) { paddingValues ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(paddingValues)
 
-        ) {
-            UserDataEntry(type = "Surname", data = "Uwe", editable = true)
-            UserDataEntry(type = "Name", data = "Uwington", editable = true)
-            UserDataEntry(type = "Email", data = "uwe@uwe.de", editable = true)
-            UserDataEntry(type = "Birthday", data = "01.01.1990", editable = false)
+            ) {
+                UserDataEntry(type = "Surname", data = "Uwe", editable = true)
+                UserDataEntry(type = "Name", data = "Uwington", editable = true)
+                UserDataEntry(type = "Email", data = "uwe@uwe.de", editable = true)
+                UserDataEntry(type = "Birthday", data = "01.01.1990", editable = false)
+            }
         }
     }
 
@@ -74,21 +86,26 @@ object ProfileScreen {
                 ),
                 decorationBox = { innerTextField ->
                     Row(
-                        verticalAlignment=Alignment.CenterVertically,
+                        verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
                             .fillMaxSize()
                     ) {
                         TextFieldDefaults.TextFieldDecorationBox(
                             value = data,
-                            label={Text(text=type, style = TextStyle(fontSize = 20.sp))},
+                            label = { Text(text = type, style = TextStyle(fontSize = 20.sp)) },
                             innerTextField = innerTextField,
                             singleLine = true,
                             enabled = true,
                             visualTransformation = VisualTransformation.None,
-                            trailingIcon = { if(editable){
-                                Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = "Editable Field",
-                                modifier=Modifier.size(80.dp))
-                            } },
+                            trailingIcon = {
+                                if (editable) {
+                                    Icon(
+                                        imageVector = Icons.Default.KeyboardArrowRight,
+                                        contentDescription = "Editable Field",
+                                        modifier = Modifier.size(80.dp)
+                                    )
+                                }
+                            },
                             interactionSource = interactionSource,
                         )
                     }
