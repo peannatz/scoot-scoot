25ff355413dc125f4059acacf617903691130400
Anna Zitzer <an.zitzer@gmail.com>
2023-07-02 11:30:42 +0200

    alcohol test functionality

--- scoot-scoot/src/main/java/com/example/scoot_scoot/android/Screens/AlcoholTest.kt
+++ scoot-scoot/src/main/java/com/example/scoot_scoot/android/Screens/AlcoholTest.kt
@@ -3,16 +3,12 @@ package com.example.scoot_scoot.android.Screens
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOut
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,11 +29,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -56,7 +50,9 @@ object AlcoholTest {
     val secondCardVisible = mutableStateOf(false)
     val thirdCardVisible = mutableStateOf(false)
     val fourthCardVisible = mutableStateOf(false)
-    val fifthCardVisible = mutableStateOf(false)
+    val countdownVisible = mutableStateOf(false)
+    val testPassed = mutableStateOf(false)
+    val testFailed = mutableStateOf(false)
     val countDown = mutableIntStateOf(5)
 
     @Composable
@@ -131,11 +127,18 @@ object AlcoholTest {
                 }) {
                 SwipableCard(
                     content = "If you fail 3 times, \nyour account will be blocked for 2 hours",
-                    imageId = R.drawable.sad, fourthCardVisible, fifthCardVisible
+                    imageId = R.drawable.sad, fourthCardVisible, countdownVisible
                 )
             }
-            AnimatedVisibility(visible = fifthCardVisible.value, enter = fadeIn()) {
+            AnimatedVisibility(visible = countdownVisible.value, enter = fadeIn()) {
                 CountdownCard()
+            }
+            AnimatedVisibility(visible = testFailed.value, enter = fadeIn()) {
+                FailedTest()
+            }
+
+            AnimatedVisibility(visible = testPassed.value, enter = fadeIn()) {
+                PassedTest(navController)
             }
         }
     }
@@ -149,7 +152,10 @@ object AlcoholTest {
         }
         Box(modifier = Modifier
             .fillMaxSize()
-            .clickable { if(countDown.value!=0) println("fail") else println("success") }
+            .clickable {
+                countdownVisible.value = false
+                if (countDown.value != 0) testFailed.value = true else testPassed.value = true
+            }
         ) {
             Text(
                 text = countDown.value.toString(), Modifier.align(Alignment.Center),
@@ -230,17 +236,58 @@ object AlcoholTest {
         }
     }
 
+    @Composable
+    private fun PassedTest(navController: NavController) {
+        Box(Modifier.fillMaxSize()) {
+            Text(
+                text = "You are not drunk. Congrats.", Modifier.align(Alignment.Center),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+        LaunchedEffect(Unit) {
+            navigateBackToMap(navController)
+        }
+    }
+
+    //TODO: remove hard coded tries when userManager is implemented
+    @Composable
+    private fun FailedTest() {
+        Box(
+            Modifier
+                .fillMaxSize()) {
+            Text(
+                text = "Oh no. You missed. \n You're either drunk or just terrible at this. \n You've got 2 more tries.",
+                Modifier.align(Alignment.Center),
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+        }
+        LaunchedEffect(Unit) {
+            restartTest()
+        }
+    }
+
+    private suspend fun restartTest() {
+        delay(3000)
+        testFailed.value=false
+        countdownVisible.value=true
+        countDown.value = 5
+    }
+
+    private suspend fun navigateBackToMap(navController: NavController) {
+        delay(1000)
+        navController.navigate(Screens.Map)
+    }
+
     private suspend fun countdownTimer() {
-        delay(1000)
-        countDown.value = countDown.value.minus(1)
-        delay(1000)
-        countDown.value = countDown.value.minus(1)
-        delay(1000)
-        countDown.value = countDown.value.minus(1)
-        delay(1000)
-        countDown.value = countDown.value.minus(1)
-        delay(1000)
-        countDown.value = countDown.value.minus(1)
-        delay(1000)
+        for (i in 1..6) {
+            delay(1000)
+            countDown.value = countDown.value.minus(1)
+        }
     }
 }
